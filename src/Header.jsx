badd669773cb40c1468b0b41f8e186a24a811775
Umer237 <umerafzal981@gmail.com>
2023-02-10 12:43:33 +0500

    update

--- src/Header.jsx
+++ src/Header.jsx
@@ -9,8 +9,8 @@ export const Header = () => {
         <ul>
             <li><a href="/home">Home</a></li>
             <li><a href="/about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="#feedback">Feedback</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li><a href="/feedback">Feedback</a></li>
         </ul> 
         </div> 
         </div>
